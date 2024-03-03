7eaf9198ed618b322129aa7ea92ff253cca66a24
Henry-Hiles <henry@henryhiles.com>
2024-03-03 13:31:03 -0500

    rename

--- models/config.ts
+++ models/config.ts
@@ -2,5 +2,5 @@ import { Data } from "dataclass"
 import Package from "./package"
 
 export default class Config extends Data {
-    packages: Array<Package>
+    installedPackages: Array<Package>
 }

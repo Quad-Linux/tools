1ff18b524258922ac9ed0fe6f1b9851283f6a565
Henry-Hiles <henry@henryhiles.com>
2024-03-08 16:18:22 -0500

    Remove unused imports

--- commands/rebuild.ts
+++ commands/rebuild.ts
@@ -1,7 +1,4 @@
-import chalk from "chalk"
-import { install, uninstall, upgrade } from "../helpers/flatpak"
 import { createCommand } from "commander"
-import ora from "ora"
 
 export default createCommand("rebuild")
     .summary("rebuild system")
--- helpers/cli.ts
+++ helpers/cli.ts
@@ -1,4 +1,4 @@
-import { exec, spawn } from "child_process"
+import { exec } from "child_process"
 import { promisify } from "util"
 
 export const execAsync = async (...cmd: string[]): Promise<string> => {

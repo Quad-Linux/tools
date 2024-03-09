8f6965f21e59cc581b9108a6d3040770f1249842
Henry-Hiles <henry@henryhiles.com>
2024-03-08 20:16:30 -0500

    Fix program name

--- cli.ts
+++ cli.ts
@@ -3,6 +3,8 @@ import { readdir } from "fs/promises"
 import { program } from "commander"
 import { join } from "path"
 
+program.name("quad")
+
 for (const command of await readdir(join(import.meta.dirname, "commands")))
     program.addCommand((await import(`./commands/${command}`)).default)
 

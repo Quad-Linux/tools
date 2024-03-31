5d80ec08d0dd6bc33ef62aea72168431e4713959
Henry-Hiles <henry@henryhiles.com>
2024-03-30 20:43:48 -0400

    Create repo

--- index.ts
+++ index.ts
@@ -3,12 +3,16 @@ import { /*configure,*/ install } from "./flatpak"
 import { execAsync, spin } from "@quados/helpers"
 
 export const createConfig = async (config: Config) => {
-    await spin("Installing packages...", setup())
-
-    await execAsync("sudo mkdir -p /var/lib/system")
-
-    await spin("Installing packages...", install(config))
+    try {
+        await spin("Setting up the system...", setup())
+        await spin("Installing packages...", install(config))
+    } finally {
+        await execAsync("pkexec umount /var/lib/system")
+    }
     // TODO: await spin("Configuring packages...", configure(config))
 }
 
-const setup = async () => {}
+const setup = async () => {
+    await execAsync("pkexec mkdir -p /var/lib/system")
+    await execAsync("pkexec mount -t tmpfs -o size=1G tmpfs /var/lib/system")
+}

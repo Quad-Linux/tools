b3dfbe4b52f5b948f40174e66d71e6081d8ec87d
Henry-Hiles <henry@henryhiles.com>
2024-06-09 22:32:15 -0400

    Add comments for plan

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Config } from "@quados/models"
-import { /*configure,*/ install } from "./flatpak"
+import { install } from "./flatpak"
 import { execAsync, spin } from "@quados/helpers"
 
 export const createConfig = async (config: Config) => {
@@ -7,12 +7,22 @@ export const createConfig = async (config: Config) => {
         await spin("Setting up the system...", setup())
         await spin("Installing packages...", install(config))
     } finally {
-        await execAsync("pkexec umount /var/lib/system")
+        await spin("Cleaning up...", cleanup())
     }
     // TODO: await spin("Configuring packages...", configure(config))
 }
 
 const setup = async () => {
+    // Create @generations/current, @overlay, @home, @var
+    // On change generation, make new subvol, build root there
+    // Make immutable snapshot of new root
+    // move old /current to be /<current date> in @generations
+    // Move immutable snapshot to be @generations/current
+
     await execAsync("pkexec mkdir -p /var/lib/system")
     await execAsync("pkexec mount -t tmpfs -o size=1G tmpfs /var/lib/system")
+}
+
+const cleanup = async () => {
+    await execAsync("pkexec umount /var/lib/system")
 }
